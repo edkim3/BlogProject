b396bf908081fb4aa3a2dd908a36a99129a6bbbd
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-13 12:27:32 -0700

    Create enums
    Made enums for post model and comment model

---
+++ BlogProject/Enums/ModerationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political propagranda")]
+        Political,
+        [Description("Offensive Language")]
+        Language,
+        [Description("Drug Reference")]
+        Drugs,
+        [Description("Threatning Speech")]
+        Threatening,
+        [Description("Sexual Content")]
+        Sexual,
+        [Description("Hate Speech")]
+        HateSpeech,
+        [Description("Targeted Shaming")]
+        Shaming
+    }
+}
