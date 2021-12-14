60e119104a602a25c62b57b913123275ad12b24e
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-14 04:54:53 -0700

    Use postgres sql
    Made changes to use postgres sql

--- BlogProject/Startup.cs
+++ BlogProject/Startup.cs
@@ -28,8 +28,14 @@ namespace BlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
+            // Update to use Npgsql
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
