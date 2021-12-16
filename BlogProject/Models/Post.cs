907c628c11c460e4975a0173e91ea25af586f5d4
edkim3 <94409377+edkim3@users.noreply.github.com>
2021-12-15 17:15:45 -0700

    Fix to display label as Blog Name
    Made changes to display label name as Blog Name

--- BlogProject/Models/Post.cs
+++ BlogProject/Models/Post.cs
@@ -14,6 +14,7 @@ namespace BlogProject.Models
     {
         public int Id { get; set; }
 
+        [Display(Name ="Blog Name")]
         public int BlogId { get; set; }
 
         public string BlogUserId { get; set; }
